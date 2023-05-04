615c87ae0c449d2afe9d6ff8ba2e270ae0eeb8c4
Amadeo Martínez Sánchez <amadeoms@correo.ugr.es>
2023-05-04 17:24:17 +0200

    grave y agudo arreglado

--- modules/audio.py
+++ modules/audio.py
@@ -3,7 +3,7 @@ from pydub import AudioSegment
 #from pydub.effects import robotize
 class audio:
     
-    #ruta="/Users/amadorcarmonamendez/Desktop/ProyectoPDS/Proyecto/data/audionuevo"
+    #ruta="/Users/amade/Documents/GitHub/Proyecto/data/audionuevo"
     
     def cargar_audio(nombre_archivo):
         ruta_archivo = nombre_archivo
@@ -11,14 +11,16 @@ class audio:
 
 
     def procesar_audio_agudo(audio):
-        nuevoaudio=audio+100
-        ruta="/Users/amadorcarmonamendez/Desktop/ProyectoPDS/Proyecto/data/audionuevo"
+        nuevo_rate = audio.frame_rate * 2
+        nuevoaudio = audio._spawn(audio.raw_data, overrides={'frame_rate':nuevo_rate})
+        ruta="/Users/amade/Documents/GitHub/Proyecto/data/audionuevo"
         nombre_salida = f"{ruta[:-4]}_agudo.wav"
         nuevoaudio.export(nombre_salida, format="wav")
 
     def procesar_audio_grave(audio):
-        nuevoaudio=audio+3
-        ruta="/Users/amadorcarmonamendez/Desktop/ProyectoPDS/Proyecto/data/audionuevo"
+        nuevo_rate = int(audio.frame_rate * 0.5)
+        nuevoaudio = audio._spawn(audio.raw_data, overrides={'frame_rate':nuevo_rate})
+        ruta="/Users/amade/Documents/GitHub/Proyecto/data/audionuevo"
         nombre_salida = f"{ruta[:-4]}_grave.wav"
         nuevoaudio.export(nombre_salida, format="wav")
 
@@ -26,7 +28,7 @@ class audio:
     def procesar_audio_robot(audio):
         x=2
         #nuevoaudio= robotize(audio, 1)
-        #ruta="/Users/amadorcarmonamendez/Desktop/ProyectoPDS/Proyecto/data/audionuevo"
+        #ruta='C:\Users\amade\Documents\GitHub\Proyecto\data\audio_nuevo'
         #nombre_salida = f"{ruta[:-4]}_grave.wav"
         #nuevoaudio.export(nombre_salida, format="wav")
 
